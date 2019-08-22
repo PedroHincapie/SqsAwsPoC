417b84e82a1ee1b22f0af1ee354c0600fe9f2598
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-08-22 16:50:11 -0500

    Modelo para retornar mensaje en caso de error

--- src/main/java/co/com/poc/sqslistener/sqs/SpringCloudSQS.java
+++ src/main/java/co/com/poc/sqslistener/sqs/SpringCloudSQS.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
@@ -17,9 +18,11 @@ public class SpringCloudSQS {
 	QueueMessagingTemplate queueMessagingTemplate;
 
 
-	@SqsListener(QUEUE_NAME)
+	@SqsListener(value= QUEUE_NAME, deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
 	public void recibirLosMensajes(String message, @Header("SenderId") String senderId) {
 		logger.info("Received message: {}, having SenderId: {}", message, senderId);
-		System.err.println("mensaje : " + message);
-	}
+		System.err.println("mensaje: " + message + "id : " + senderId);
+		throw new RuntimeException("Pedro estas el error por ende va para la cola");
+
+	}	
 }
