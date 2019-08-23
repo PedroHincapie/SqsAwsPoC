1d3a41bf86c8b3b028199e762ece1920e50e1964
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-08-23 09:31:09 -0500

    Separecion de responsabilidad

--- src/main/java/co/com/poc/sqslistener/sqs/SpringCloudSQS.java
+++ src/main/java/co/com/poc/sqslistener/sqs/SpringCloudSQS.java
@@ -28,6 +28,7 @@ public class SpringCloudSQS {
 	static final String QUEUE_AL_FALLA_CONSUMIDOR = "al-fallar-consumidor.fifo";
 	static final String QUEUE_AL_NO_EXISTIR_MENSAJE= "al-no-encontrar-mensajes-en-cola.fifo";
 	static final Integer TIEMPO_DE_ESPERA_SEGUNDOS = 20;
+	static final String TIEMPO_POR_DEFAULT = "30";
 
 	@Autowired
 	QueueMessagingTemplate queueMessagingTemplate;
@@ -36,51 +37,74 @@ public class SpringCloudSQS {
 	//	estrategia diseñada para eventualidades de fallos en el consumidor para una cola. 
 	@SqsListener(value= QUEUE_AL_FALLA_CONSUMIDOR, deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
 	public void recibirMensajesYErrorEnCliente(String message, @Header("SenderId") String senderId) {
-		logger.info("Received message: {}, having SenderId: {}", message, senderId);
+		logger.info("Mensaje a procesar : {}, having SenderId: {}", message, senderId);
 
 		//Validar el estado de la impresora
 		if(IMPRESORA_EN_FALLO) {
-			//Crear el cliente
-			final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
-			//Consultar la URL de la cola que se ajustara
-			final String queueUrl= sqs.getQueueUrl(QUEUE_AL_FALLA_CONSUMIDOR).getQueueUrl();
-
-			//Contruir lista con el atributo de la cola a necesitar
-			List<String> attributeNames = new ArrayList<>();
-			attributeNames.add("VisibilityTimeout");
-
-			//Crear Objeto del request con el objeto a solicitar
-			GetQueueAttributesRequest requestAtri = new GetQueueAttributesRequest(queueUrl);
-			requestAtri.setAttributeNames(attributeNames);
-
-			//Realizar la solicitud con el cliente y obtener el valor
-			Integer tiempoVisibilidad = Integer.parseInt(sqs.getQueueAttributes(requestAtri).getAttributes().get("VisibilityTimeout"));
-
-			System.err.println("Este es el tiempo :" + tiempoVisibilidad);
-
-			//Incrementar valor
-			Integer tiempoVisibilidadIncrementado = tiempoVisibilidad + TIEMPO_DE_ESPERA_SEGUNDOS;
-
-			System.err.println("Este es el tiempo resultante :" + tiempoVisibilidadIncrementado);
-
-			//Crear objeto con el valor de la visibilidad
-			final SetQueueAttributesRequest request = new SetQueueAttributesRequest()
-					.withQueueUrl(queueUrl)
-					.addAttributesEntry(QueueAttributeName.VisibilityTimeout
-							.toString(), tiempoVisibilidadIncrementado.toString());
-
-			//Seterar el valor en aws
-			sqs.setQueueAttributes(request);
-
+			incrementarTiemporDeVisibilidadDeMensaje();
 			throw new RuntimeException("Pedro estas el error por ende va para la cola");
 		}
 
-		System.err.println("mensaje exitoso: " + message + "id : " + senderId);
+		resetTiemporDeVisibilidadDeMensaje();
+		logger.info("Mensaje exitoso y  procesado : {}, having SenderId: {}", message, senderId);
+	}
 
 
+	private void incrementarTiemporDeVisibilidadDeMensaje() {
+		//Crear el cliente
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
+		//Consultar la URL de la cola que se ajustara
+		final String queueUrl= sqs.getQueueUrl(QUEUE_AL_FALLA_CONSUMIDOR).getQueueUrl();
 
+		//Contruir lista con el atributo de la cola a necesitar
+		List<String> attributeNames = new ArrayList<>();
+		attributeNames.add("VisibilityTimeout");
+
+		//Crear Objeto del request con el objeto a solicitar
+		GetQueueAttributesRequest requestAtri = new GetQueueAttributesRequest(queueUrl);
+		requestAtri.setAttributeNames(attributeNames);
+
+		//Realizar la solicitud con el cliente y obtener el valor
+		Integer tiempoVisibilidad = Integer.parseInt(sqs.getQueueAttributes(requestAtri).getAttributes().get("VisibilityTimeout"));
+
+		logger.info("Tiempo de visibilidad actual : {} ", tiempoVisibilidad);
+
+		//Incrementar valor
+		Integer tiempoVisibilidadIncrementado = tiempoVisibilidad + TIEMPO_DE_ESPERA_SEGUNDOS;
+
+		logger.info("Tiempo de visibilidad resultante : {} ", tiempoVisibilidadIncrementado);
+
+		//Crear objeto con el valor de la visibilidad
+		final SetQueueAttributesRequest request = new SetQueueAttributesRequest()
+				.withQueueUrl(queueUrl)
+				.addAttributesEntry(QueueAttributeName.VisibilityTimeout
+						.toString(), tiempoVisibilidadIncrementado.toString());
+
+		//Seterar el valor en aws
+		sqs.setQueueAttributes(request);
+
+		logger.info("Valor incrementado exitosamente");
+	}
+
+	private void resetTiemporDeVisibilidadDeMensaje() {
+		//Crear el cliente
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+		//Consultar la URL de la cola que se ajustara
+		final String queueUrl= sqs.getQueueUrl(QUEUE_AL_FALLA_CONSUMIDOR).getQueueUrl();
+
+		//Crear objeto con el valor de la visibilidad
+		final SetQueueAttributesRequest request = new SetQueueAttributesRequest()
+				.withQueueUrl(queueUrl)
+				.addAttributesEntry(QueueAttributeName.VisibilityTimeout
+						.toString(), TIEMPO_POR_DEFAULT);
+
+		//Seterar el valor en aws
+		sqs.setQueueAttributes(request);
+
+		logger.info("Valor Reset exitosamente");
 	}
 
 	//	Este método tiene por objetivo presentar la estrategia diseñada para 
